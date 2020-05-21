2e12a7c79663ce0e037b535a00c9dbdf68b8f050
Fabricio <fabricio@Fabricios-MacBook-Pro.local>
2020-05-21 19:01:32 -0300

    Modificaciones en los Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Component
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistersComponent } from './login/registers.component';
+
+// Rutas
+import { APP_ROUTES } from 'src/app.routes';
+
+// Modulos
+import { PagesModule } from './pages/pages.module';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistersComponent,
   ],
   imports: [
+    PagesModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
